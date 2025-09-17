b69de567961ab86574163a11390f134b65da2ff7
Yahia El Afia <yahiaelafia1@gmail.com>
2025-09-17 15:17:22 +0100

    chpitre 9

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let age = 20;
+/*let age = 20;
 
 if (age < 18) {
   console.log("Vous êtes mineur.");
@@ -6,4 +6,13 @@ if (age < 18) {
   console.log("Vous venez d’atteindre la majorité !");
 } else {
   console.log("Vous êtes majeur.");
+}
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) continue; // sauter le 5
+    console.log("Nombre : " + i);
+  }
+*/
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) continue; // sauter le 5
+  console.log("Nombre : " + i);
 }
