191ba58c372b596c6bbad6694eda6edc47e667b5
Yahia El Afia <yahiaelafia1@gmail.com>
2025-09-17 13:37:27 +0100

    Chapitre 3 – Conditions et logique booléenne

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let nombre1 = 10;
-let nombre2 = 5;
-let somme = nombre1 + nombre2;
-console.log("La somme est : " + somme);
+let age = 20;
 
-function addition(a, b) {
-    return a + b;
-  }
-  
-  let somme2= addition(8, 5);
-  console.log("La somme2 est : " + somme2);
-  
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d’atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
